e8df664d261b18a7b4af59bc7417e17830a5ed90
rossana87 <rossana.ventrella@gmail.com>
2026-09-22 14:28:17 +0100

    comment out

--- callbacks.js
+++ callbacks.js
@@ -216,8 +216,8 @@ function majority(array, callback) {
 }
 
 // /*** Uncomment these to check your work! ***/
-const isOdd = function (num) {
-  return num % 2 === 1;
-};
-console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
-console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
+// const isOdd = function (num) {
+//   return num % 2 === 1;
+// };
+// console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+// console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
